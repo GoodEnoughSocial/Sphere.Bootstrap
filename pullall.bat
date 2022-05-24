b0be1656cf170e43311b80d44fc3cdb571b8acff
Chad Jessup <chad.jessup@gmail.com>
2022-05-24 01:14:48 -0700

    very basic clone/pull scripts

---
+++ pullall.bat
@@ -0,0 +1,51 @@
+cd Sphere.Timeline.API
+git pull
+cd ..
+
+cd Sphere.Accounts.API
+git pull
+cd ..
+
+cd Sphere.Administration.API
+git pull
+cd ..
+
+cd Sphere.ApiGateway.API
+git pull
+cd ..
+
+cd Sphere.Auth.API
+git pull
+cd ..
+
+cd Sphere.Bootstrap
+git pull
+cd ..
+
+cd Sphere.Branding.API
+git pull
+cd ..
+
+cd Sphere.Health.API
+git pull
+cd ..
+
+cd Sphere.Media.API
+git pull
+cd ..
+
+cd Sphere.Messages.API
+git pull
+cd ..
+
+cd Sphere.Profiles.API
+git pull
+cd ..
+
+cd Sphere.Relationships.API
+git pull
+cd ..
+
+cd Sphere.Spheres.API
+git pull
+cd ..
